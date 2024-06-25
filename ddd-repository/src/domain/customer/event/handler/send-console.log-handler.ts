d4547e8395a60ae3c2c89bf2114c5d2e09608828
Kevin Beltrao de Melo <kbmelo@outlook.com>
2024-06-25 17:30:03 -0300

    renaming CustomerCreatedEvent

--- ddd-repository/src/domain/customer/event/handler/send-console.log-handler.ts
+++ ddd-repository/src/domain/customer/event/handler/send-console.log-handler.ts
@@ -1,10 +1,10 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
-import ProductCreatedEvent from "../customer-created.event";
+import CustomerCreatedEvent from "../customer-created.event";
 
 export default class SendConsoleLogHandler
-  implements EventHandlerInterface<ProductCreatedEvent>
+  implements EventHandlerInterface<CustomerCreatedEvent>
 {
-  handle(event: ProductCreatedEvent): void {
+  handle(event: CustomerCreatedEvent): void {
     const { id, name, Address } = event.eventData;
     console.log(`Endereço do cliente: ${id}, ${name} alterado para: ${Address}`); 
   }
